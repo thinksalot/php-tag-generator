dcaf30c76ee51f8592bf46eb69f978269b305314
thinksalot <thapa.aalok@gmail.com>
2013-11-01 21:36:42 +0545

    ignoring git, .js files

--- tags.sh
+++ tags.sh
@@ -4,10 +4,13 @@
 
 ctags-exuberant -f tags \
 	-h \".php\" -R \
+  --fields=+l \
 	--exclude=\"\.svn\" \
 	--totals=yes \
 	--tag-relative=yes \
 	--PHP-kinds=+cf \
 	--regex-PHP='/abstract class ([^ ]*)/\1/c/' \
 	--regex-PHP='/interface ([^ ]*)/\1/c/' \
-	--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/'
+	--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/' \
+	--exclude=\"\.git\" \
+	--exclude="*.js"
