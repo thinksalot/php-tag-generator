f45b1acc345ba6b9702ac747d0233589e69e76ef
thinksalot <thapa.aalok@gmail.com>
2014-01-13 15:07:48 +0545

    filtering files with find

--- tags.sh
+++ tags.sh
@@ -2,15 +2,24 @@
 # originally from
 # http://www.mwop.net/blog/134-exuberant-ctags-with-PHP-in-Vim.html
 
-ctags-exuberant -f tags \
-	-h \".php\" -R \
-  --fields=+l \
-	--exclude=\"\.svn\" \
+# file which contains a list of files to proce
+fileList=indexme
+
+# finds files, excluding certain folders and saves it to alist
+find . \
+	-name '*.php' \
+	! -path '*external*' \
+	! -path '*doc_generator*' \
+	! -path '*tests/cache*' \
+	! -path '*tests/vendor*' \
+	! -path '*firefor-profile*' > $fileList
+
+# build ctags from the list
+ctags-exuberant -L $fileList \
+    --fields=+l \
 	--totals=yes \
 	--tag-relative=yes \
 	--PHP-kinds=+cf \
 	--regex-PHP='/abstract class ([^ ]*)/\1/c/' \
 	--regex-PHP='/interface ([^ ]*)/\1/c/' \
 	--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/' \
-	--exclude=\"\.git\" \
-	--exclude="*.js"
