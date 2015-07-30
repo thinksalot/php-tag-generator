a76f528e8cff114682ebc2f64e7af42b159762d3
thinksalot <thapa.aalok@gmail.com>
2015-07-30 08:49:01 +0545

    adding some echo, ignoring pdf lib path

--- tags.sh
+++ tags.sh
@@ -5,6 +5,8 @@
 # file which contains a list of files to proce
 fileList=indexme
 
+echo 'Building list of file to process...'
+
 # finds files, excluding certain folders and saves it to alist
 find . \
 	-name '*.php' \
@@ -12,7 +14,10 @@ find . \
 	! -path '*doc_generator*' \
 	! -path '*tests/cache*' \
 	! -path '*tests/vendor*' \
+	! -path '*tcpdf*' \
 	! -path '*firefor-profile*' > $fileList
+
+echo 'Preparing tag file...'
 
 # build ctags from the list
 ctags-exuberant -L $fileList \
@@ -23,3 +28,8 @@ ctags-exuberant -L $fileList \
 	--regex-PHP='/abstract class ([^ ]*)/\1/c/' \
 	--regex-PHP='/interface ([^ ]*)/\1/c/' \
 	--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/' \
+
+# remove filelist
+rm $fileList
+
+echo 'Done !'
